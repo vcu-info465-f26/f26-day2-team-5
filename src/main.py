f2e032e97a76994019f2d442f2036dacf640b088
Anariveracantu <a.riveracantu@gmail.com>
2026-08-28 01:07:23 +0000

    Update analyze and main

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import build_db
 import chart
 import fetch
-
+import analyze
 # YOUR WORK, STEP 1 OF 2
 # Uncomment this once you have created src/analyze.py on your branch:
 #
@@ -16,7 +16,8 @@ def main():
     forecast = fetch.api_call()
     build_db.save_to_db(forecast)
     chart.make_chart()
-
+    data=analyze.get_max()
+    print(data)
 
     print("Done. Look in output/ for chart.png and weather.db,")
     print("then run 'git status' and notice that neither one is listed.")
