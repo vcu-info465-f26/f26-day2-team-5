baff954a16d8bf8c92cf5fd8706af1c7d459780e
nelsonjm5 <nelsonjm5@vcu.edu>
2026-09-17 13:44:03 +0000

    Add Sprint 1 snapshot and schema

--- src/fetch_events.py
+++ src/fetch_events.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 
 # Function that gets event data from Ticketmaster
 def get_events():
@@ -24,6 +25,10 @@ def get_events():
 
     # Converts the JSON response into a Python dictionary
     data = response.json()
+    # Saves a snapshot of the event data
+    with open("data/events_snapshot.json", "w") as file:
+        json.dump(data, file, indent=4)
+
 
     # Shows the top-level keys in the response
     print(data.keys())
